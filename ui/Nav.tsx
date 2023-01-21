f3af8f3225b78552ffb90d4e8e3c7ab67910e3e4
KiSEiGitHub <tommusique974@gmail.com>
2023-01-21 01:55:06 +0100

    toggle theme

--- ui/Nav.tsx
+++ ui/Nav.tsx
@@ -1,15 +1,18 @@
-import { Box, Button, useColorMode } from "@chakra-ui/react";
-import React from "react";
+import { Box, Button, useColorMode, useColorModeValue } from "@chakra-ui/react";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 function Nav() {
-  const {colorMode, toggleColorMode} = useColorMode()
-  return (
-    <Box>
-      <Button onClick={toggleColorMode}>
-        {colorMode === 'light' ? 'Dark' : 'Light'}
-      </Button>
-    </Box>
-  );
+   const { colorMode, toggleColorMode } = useColorMode();
+   return (
+      <Box pos="fixed" right={10} top={10} zIndex={2}>
+         <Button
+            onClick={toggleColorMode}
+            colorScheme={useColorModeValue("purple", "orange")}
+         >
+            {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
+         </Button>
+      </Box>
+   );
 }
 
 export default Nav;
